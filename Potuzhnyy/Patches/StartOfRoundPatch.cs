8bc435c5199d207556d83bceee9626d29d13faa9
Maksym Niechaiev <34244452+niechaiev@users.noreply.github.com>
2025-06-15 14:02:33 +0300

    V 1.0.2

---
+++ Potuzhnyy/Patches/StartOfRoundPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Potuzhnyy.Patches
+{
+    internal static class StartOfRoundPatch
+    {
+        [HarmonyPatch(typeof(StartOfRound), nameof(Start))]
+        [HarmonyPostfix]
+        [HarmonyPriority(Priority.High)]
+        private static void Start(StartOfRound __instance)
+        {
+            var cabinet = GameObject.Find("StorageCloset");
+            if (cabinet && cabinet.transform.childCount > 1 && cabinet.transform.GetChild(0).childCount > 0 &&
+                cabinet.transform.GetChild(1).childCount > 0
+                && cabinet.transform.GetChild(0).GetChild(0).TryGetComponent<InteractTrigger>(out _)
+                && cabinet.transform.GetChild(1).GetChild(0).TryGetComponent<InteractTrigger>(out _))
+            {
+                Object.Destroy(cabinet.transform.GetChild(0).gameObject);
+                //Object.Destroy(cabinet.transform.GetChild(1).gameObject);
+            }
+        }
+    }
+}
---
+++ Potuzhnyy/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using LethalLib.Modules;
+using Potuzhnyy.Patches;
+using UnityEngine;
+
+namespace Potuzhnyy
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            
+            harmony.PatchAll(typeof(Plugin));
+            harmony.PatchAll(typeof(StartOfRoundPatch));
+
+            RegisterItems();
+        }
+
+        private AssetBundle bundle;
+        private void RegisterItems()
+        {
+            string assetDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "itemmod");
+            bundle = AssetBundle.LoadFromFile(assetDir);
+
+            RegisterItem("Assets/MaxonPillowItem.asset", 15);
+            RegisterItem("Assets/ValikPillowItem.asset", 20);
+        }
+
+        private void RegisterItem(string path, int rarity)
+        {
+            Item item = bundle.LoadAsset<Item>(path);
+
+            NetworkPrefabs.RegisterNetworkPrefab(item.spawnPrefab);
+            Utilities.FixMixerGroups(item.spawnPrefab);
+            Items.RegisterScrap(item, rarity, Levels.LevelTypes.All);
+
+            Logger.LogInfo(path + " registered");
+        }
+    }
+}
